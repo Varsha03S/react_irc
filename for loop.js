14081697248cad48b6f72f92538a75db2daf7d6c
Varsha03S <118422969+Varsha03S@users.noreply.github.com>
2023-12-10 11:35:58 +0530

    added files

---
+++ for loop.js
@@ -0,0 +1,8 @@
+
+let sum = 0;
+const n = 100;
+for(let i = n; i >= 1; i-- ) {
+    sum += i; 
+}
+
+console.log('sum:',sum);
---
+++ script.js
@@ -0,0 +1,10 @@
+function manip()
+{
+    var btn = document.getElementById("changer");
+    btn.style.backgroundColor = "rgb(223, 33, 33";
+}
+
+
+	  
+      
+     
