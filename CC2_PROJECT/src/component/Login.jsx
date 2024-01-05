0eea69a7d839f6c264ecce34d235baac4be62a8c
Varsha03S <118422969+Varsha03S@users.noreply.github.com>
2024-01-05 15:47:45 +0530

    axios implemented

--- CC2_PROJECT/src/component/Login.jsx
+++ CC2_PROJECT/src/component/Login.jsx
@@ -1,25 +1,72 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
 import '../assets/Login.css';
 
-import {Link} from 'react-router-dom';
-function Login()
-{
-    return(
-  
-        <div class="login-container">
-    <h2>Login</h2>
-    <br/>
-      <br/>
-    <form>
-      <input type="email" placeholder="Email" id="email" name="email"></input>
-      <input type="password" placeholder="Password" id="password" name="password"></input>
-      <center><Link to='/home'><button type="submit">Login</button></Link></center>
-      <br></br>
-      <center><p>Don't you have an account?  <Link to='/register'>Sign up</Link></p></center>
-    </form>
-  </div>
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    );
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.get("http://localhost:3001/users?email="+email );
+      if (response.data.length === 0) {
+        window.alert("User doesn't exist");
+        return;
+      }
+
+      const user = response.data[0];
+
+      if (user.password === password) {
+        window.alert("Login successful");
+        window.location.href = '/home';
+      } else {
+        window.alert("Incorrect password. Try again!");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <div class="login-container">
+      <h2>Login</h2>
+      <br />
+      <br />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          minLength="6"
+        />
+        <center>
+          <button type="submit">Login</button>
+        </center>
+        <br />
+        <center>
+          <p>
+            Don't have an account? <Link to='/register'>Sign up</Link>
+          </p>
+        </center>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
-
--- CC2_PROJECT/src/component/Registration.jsx
+++ CC2_PROJECT/src/component/Registration.jsx
@@ -1,27 +1,113 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
 import '../assets/Registration.css';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import {Link} from 'react-router-dom';
-function Registration(){
-    return(
-      <div>
-        <Navbar/>
-    <div class="register-container">
-      <h2>Sign Up</h2>
-      <br/>
-      <br/>
-      <form>
-        <input type="text" placeholder="Username" id="userName" name="userName"></input>
-        <input type="email" placeholder="Email" id="email" name="email"></input>
-        <input type="password" placeholder="Password" id="password" name="password"></input>
-        <input type="password" placeholder="Confirm Password" id="confirmPassword" name="confirmPassword"></input>
-        <button type="submit">Sign Up</button>
-      <br/>
-      <center><p>Already have an account?  <Link to='/login'>Login</Link></p></center>
-    </form>
-  </div>
-   <Footer/>
-  </div>
-);
+
+function Registration() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+
+  //   // Add your registration logic here, e.g., making an API call to register the user
+
+  //   try {
+  //     // Example: Make an API call to register the user
+  //     const response = await axios.post("http://localhost:3001/register", {
+  //       username,
+  //       email,
+  //       password,
+  //       confirmPassword,
+  //     });
+
+  //     // Handle the response as needed
+  //     console.log("Registration successful:", response.data);
+
+  //     // Redirect to login page after successful registration
+  //     window.location.href = '/login';
+  //   } catch (error) {
+  //     console.error("Error:", error);
+  //   }
+  // };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const isValid = validateForm();
+
+    if (isValid) {
+      window.alert("Registration succesful. Move to Login Page!");
+      window.location.href = '/';
+    } else {
+      window.alert("Please fill in all the required fields properly.");
+    }
+  };
+
+  const validateForm = () => {
+    return username && email && password && confirmPassword && (password === confirmPassword);
+  };
+  return (
+    <div>
+      <Navbar />
+      <div class="register-container">
+        <h2>Sign Up</h2>
+        <br />
+        <br />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Username"
+            id="userName"
+            name="userName"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            minLength="6"
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            id="confirmPassword"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+            minLength="6"
+          />
+          <button type="submit">Sign Up</button>
+          <br />
+          <center>
+            <p>
+              Already have an account? <Link to='/login'>Login</Link>
+            </p>
+          </center>
+        </form>
+      </div>
+      <Footer />
+    </div>
+  );
 }
+
 export default Registration;
